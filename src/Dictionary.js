9d6980dac86583d5ca969c870f74c7066021a296
Samantha Lima Luna Oriá <samanthalimaadv@yahoo.com.br>
2021-04-21 16:05:45 +0200

    creating the componets(meaning, results)

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results"
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
 
   function handleResponse(response){
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
+    //documentation: https://dictionaryapi.dev/
+
     const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    console.log(apiUrl)
+    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -26,6 +30,7 @@ export default function Dictionary() {
       <form onSubmit={search}> 
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
