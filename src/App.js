9994fa1a559863da55f3e884cc578ed707cb0a6c
Samantha Lima Luna Oriá <samanthalimaadv@yahoo.com.br>
2021-04-22 15:57:09 +0200

    added a link to github

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small> Coded by Samantha Oriá </small>
+          <small> Coded by 
+            <a href="https://github.com/samanthaoria/dictionary-project.git"> Samantha Oriá </a>
+            </small>
         </footer>
       </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,9 +26,9 @@ export default function Dictionary() {
   }
 
   return ( 
-    <div className="Dictionary">
+    <div className="Dictionary text-center">
       <form onSubmit={search}> 
-        <input type="search" onChange={handleKeywordChange} />
+        <input type="search" onChange={handleKeywordChange}  /> 
       </form>
       <Results results={results} />
     </div>
