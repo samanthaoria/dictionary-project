70662c9650f19771ef107c7b0055f60e155da503
Samantha Lima Luna Oriá <samanthalimaadv@yahoo.com.br>
2021-05-17 14:17:07 +0200

    adding some style

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,6 +4,7 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <ul className="Synonyms">
+        <strong>Synonyms:</strong>{" "}
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}> {synonym}</li>;
         })}
