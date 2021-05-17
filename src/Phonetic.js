e5b63f49b5113d348c58a74c2dccd10cfeb0cf3b
Samantha Lima Luna Oriá <samanthalimaadv@yahoo.com.br>
2021-05-17 16:17:17 +0200

    fixing bugs

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -6,7 +6,7 @@ export default function Phonetic(props){
     <div className="Phonetic">
       {" "}
       <a href={props.phonetic.audio} target="_blank">
-      {/* <Listen />   */} Listen 
+      Listen 
       <span> <i class="fas fa-volume-up"></i> </span>
       </a>
       <br />
