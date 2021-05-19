2cde906205a817f2eb123cb58ef4059e891fdff4
Samantha Lima Luna Oriá <samanthalimaadv@yahoo.com.br>
2021-05-19 14:51:19 +0200

    improved the css and added photos api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,27 +1,39 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Results from "./Results"
+import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
-  function handleResponse(response){
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(){
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  } 
+
+  function search() {
     //documentation: https://dictionaryapi.dev/
 
     const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey =
+      "563492ad6f91700001000001d5bad6b6e7664d2ab9a68ebc7f635988";
+    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    const headers = {Authorization: `Bearer ${pexelsApiKey}`};
+    axios.get(pexelsApiUrl, {headers: headers,}).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
-    event.preventDefault(); 
+    event.preventDefault();
     search();
   }
 
@@ -29,22 +41,30 @@ export default function Dictionary(props) {
     setKeyword(event.target.value);
   }
 
-  function load(){
+  function load() {
     setLoaded(true);
     search();
   }
 
-  if(loaded){
+  if (loaded) {
     return (
       <div className="Dictionary">
         <section>
           <h1> What word do you want yo look up?</h1>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword} />
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
-          <div className="hint"> Suggested words: sunrise, beer, pilates...</div>
+          <div className="hint">
+            {" "}
+            Suggested words: sunrise, beer, pilates...
+          </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
