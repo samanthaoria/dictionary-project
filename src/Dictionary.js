9a0783ad55bc691ea8751e11253d1920b3ec88f4
Samantha Lima Luna Oriá <samanthalimaadv@yahoo.com.br>
2021-06-16 10:32:02 +0200

    final changes

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import Photos from "./Photos";
@@ -6,9 +6,39 @@ import "./Dictionary.css";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
-  const [results, setResults] = useState(null);
+  const [results, setResults] = useState([]);
+  const [photos, setPhotos] = useState([]);
   const [loaded, setLoaded] = useState(false);
-  const [photos, setPhotos] = useState(null);
+
+  function search() {
+    //documentation: https://dictionaryapi.dev/
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios
+      .get(apiUrl)
+      .then(handleDictionaryResponse)
+      .catch((e) => {
+        console.error(e);
+        setResults([]);
+      });
+
+    const pexelsApiKey =
+      "563492ad6f91700001000001d5bad6b6e7664d2ab9a68ebc7f635988";
+    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    const headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    return axios
+      .get(pexelsApiUrl, { headers })
+      .then(handlePexelsResponse)
+      .catch((e) => {
+        console.error(e);
+        setPhotos([]);
+      });
+  }
+
+  useEffect(() => {
+    if (!loaded) {
+      search().finally(() => setLoaded(true));
+    }
+  }, [search]);
 
   function handleDictionaryResponse(response) {
     setResults(response.data[0]);
@@ -16,20 +46,6 @@ export default function Dictionary(props) {
 
   function handlePexelsResponse(response) {
     setPhotos(response.data.photos);
-  } 
-
-  function search() {
-    //documentation: https://dictionaryapi.dev/
-
-    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(handleDictionaryResponse);
-
-    const pexelsApiKey =
-      "563492ad6f91700001000001d5bad6b6e7664d2ab9a68ebc7f635988";
-    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
-    const headers = {Authorization: `Bearer ${pexelsApiKey}`};
-    axios.get(pexelsApiUrl, {headers: headers,}).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -41,34 +57,27 @@ export default function Dictionary(props) {
     setKeyword(event.target.value);
   }
 
-  function load() {
-    setLoaded(true);
-    search();
-  }
-
-  if (loaded) {
-    return (
-      <div className="Dictionary">
-        <section>
-          <h1> What word do you want to look up?</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="search"
-              onChange={handleKeywordChange}
-              defaultValue={props.defaultKeyword}
-            />
-          </form>
-          <div className="hint">
-            {" "}
-            Suggested words: sunrise, beer, pilates...
-          </div>
-        </section>
-        <Results results={results} />
-        <Photos photos={photos} />
-      </div>
-    );
-  } else {
-    load();
-    return "Loading";
-  }
+  return loaded ? (
+    <div className="Dictionary">
+      <section>
+        <h1> What word do you want to look up?</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="defaultWord"
+            type="search"
+            onChange={handleKeywordChange}
+            defaultValue={props.defaultKeyword}
+          />
+        </form>
+        <div className="hint">
+          Write the word and press enter. Suggested words: sunrise, beer,
+          pilates...
+        </div>
+      </section>
+      <Results results={results} />
+      <Photos photos={photos} />
+    </div>
+  ) : (
+    "Loading..."
+  );
 }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Phonetics.css";
 
-export default function Phonetic(props){
+export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
         <span>
           {" "}
-          <i class="fas fa-volume-up"> </i>{" "}
+          <i className="fas fa-volume-up"> </i>{" "}
         </span>
       </a>
       <span className="text">{props.phonetic.text} </span>
